04f48f0903018f8b47a812aee3c5d5599de3c322
hiko-ogeqq <knhk0419@gmail.com>
2020-12-19 12:34:21 +0900

    2-5 modelandview

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,19 +1,20 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class DemoController {
 	@RequestMapping("/{num}")
-	public String index(@PathVariable int num, Model model) {
-		int sum = 0;
-		for(int i = 1 ; i <= num ; i++) {
-			sum += i;
-		}
-		model.addAttribute("msg", "sum=" + sum);
-		return "index";
+	public ModelAndView index(@PathVariable int num, ModelAndView mav) {
+			int sum = 0;
+			for(int i = 1 ; i <= num ; i++) {
+					sum += i;
+			}
+			mav.addObject("msg", "sum=" + sum);
+			mav.setViewName("index");
+			return mav;
 	}
 }
